f9798031af2ee9570797e13ea970fd49ddd2cf7c
Marmat <eadbo001@edu.xamk.fi>
2025-01-21 11:37:22 +0200

    cleaned up shit

--- Botham_Adam_ProgrammingProject/Botham_Adam_ProgrammingProject.cpp
+++ Botham_Adam_ProgrammingProject/Botham_Adam_ProgrammingProject.cpp
@@ -1,8 +1,4 @@
 //Programmingproject - Botham Adam
-// 
-//Let's start with instructions: replace "Surname Forename" with your own name
-//and then delete instructions from the file that you don't need anymore!
-
 
 //include iostream sourcecode(ex. cout and cin -functions)
 #include <iostream>
@@ -10,28 +6,38 @@
 #include <Windows.h>
 #include <string>
 
-//Tell the code that we are using the namespace std (C++ standard librabry functions are included in this namespace)
 using namespace std;
 
 //In C/C++ -programs functions have to be declared before defining them
 void Part1();
 void Part2();
-void Part3();
+
 
 //The main() function is the start of execution in every program and has to be included them:
 int main()
-{
-	//Finnish letters
+{	
 	SetConsoleOutputCP(1252);
 	SetConsoleCP(1252);
 
+	Part1();
+	
+	Part2();
+	
+	
+	
+	cout << "\nThe program has ended and shutdown." << endl;
+}
+
+//Input asked info and outputs given info
+void Part1()
+{
 	string name;
 	int age;
 	int studentNum;
 	float height;
 	float weight;
 
-	//The cout-function outputs
+	
 	cout << "The program starts: \n" << endl;
 
 	cout << "Input the following:\nName: "; cin >> name;
@@ -40,42 +46,19 @@ int main()
 	cout << "Height: "; cin >> height;
 	cout << "Weight: "; cin >> weight;
 
-
-
-	//Inside functions you can call for other functions in any order.    
-	Part1();
-	//The program "jumps" to execute the called function and then returns to the line after the call.
-	//Let's call another function
-	Part2();
-	//And a third one.
-	Part3();
-
-
-	//Programs are usually written to run continuously and typically using a loop.
-	//Without a loop, the program ends when there are no more lines to execute in the main function
-	cout << "\nThe program has ended and shutdown." << endl;
+	cout << "\nGiven info:\nName: " << name << "\nAge: " << age << "\nStudent ID: " << studentNum << "\nHeight: " << height << "\nWeight: " << weight << endl;	
 }
 
-//set Num variables and sum them together and then output the sum
-void Part1()
+//input grant months per year and amount and outputs annual grant amount
+void Part2()
 {
 	int months;
 	float grantAmount;
 	float grantPerYear;
 	cout << "\nStudy grant months per year\nMonths: "; cin >> months;
-	cout << "\nMonthly grant amount: "; cin >> grantAmount;
+	cout << "Monthly grant amount: "; cin >> grantAmount;
 	grantPerYear = months * grantAmount;
-	cout << "\nAnnual study grant amount: " << grantPerYear << "€";
+	cout << "Annual study grant amount: " << grantPerYear << "€" << endl;
 }
 
-//two integer variables that switch values
-void Part2()
-{
-	
-}
-
-void Part3()
-{
-
-}
 
