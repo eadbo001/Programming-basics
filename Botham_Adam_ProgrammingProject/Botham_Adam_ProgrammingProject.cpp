5daf094af7c1994695075537775d63c57ca6561d
Marmat <eadbo001@edu.xamk.fi>
2025-01-30 21:02:54 +0200

    section 5 and switch cases and help menu

--- Botham_Adam_ProgrammingProject/Botham_Adam_ProgrammingProject.cpp
+++ Botham_Adam_ProgrammingProject/Botham_Adam_ProgrammingProject.cpp
@@ -10,47 +10,40 @@
 using namespace std;
 
 //In C/C++ -programs functions have to be declared before defining them
-void Part1();
-void Part2();
-void Part3();
+void ShowMenu();
+void PersonalID();
+void AnnualGrant();
+void ChooseFunction();
+void PrimeCheck();
+
 
 
 //The main() function is the start of execution in every program and has to be included them:
 int main()
 {	
-	int choosePart;
+	//char type variable are written inside sinlge '' parenthesis and string type are in double ""
+	char choosePart;
 	SetConsoleOutputCP(1252);
 	SetConsoleCP(1252);
 	cout << "The program starts:" << endl;
-	cout << "\n-------------------------\n" << endl;
-	do
+	cout << "\n-------------------------" << endl;
+	do //do while loop executes once before checking the condition for the loop
 	{
 
+	 ShowMenu();
 	
-	cout << "Choose which Part/Function to execute:" << endl << endl
-		 << "1: Personal ID" << endl
-		 << "2: Annual Study Grant" << endl
-		 << "3: Choose between 2 functions" << endl	
-		 << "9: Quit" << endl
-		 << "Enter your choice: ";
+	 cout << "Enter your choice: ";
 	 cin >> choosePart;
+	 //switch case is a great way to check for different input to a variable and executing different code based on it
+	 //It is technically similar to if else if statements but it looks a lot nicer and is more readable
 	switch (choosePart) {
-		case 1: {
-			cout << "\n*** PART 1***" << endl;
-			Part1();
-			break;
-		}
-		case 2: {
-			cout << "\n*** PART 2***" << endl;
-			Part2();
-			break;
-		}
-		case 3: {
-			cout << "\n*** PART 3***" << endl;
-			Part3();
-			break;
-		}
-		case 9: {
+		case '1': { PersonalID(); break;}
+		case '2': { AnnualGrant(); break;}
+		case '3': { ChooseFunction(); break; }
+		case '4': { ShowMenu(); break;}
+		case '5': { PrimeCheck(); break;}
+
+		case 'q': case 'Q': {
 			cout << "\nBYE BYE :)" << endl;
 			break;
 		}
@@ -58,14 +51,25 @@ int main()
 			cout << "\nInvalid choice :( Try again." << endl;
 		}
 	}
-	} while (choosePart !=9);
+	} while (choosePart !='q' && choosePart !='Q');
 	
 	cout << "\n-------------------------\n" << endl;
 	cout << "The program has ended and shutdown." << endl;
 }
 
+void ShowMenu() {
+	cout << "\nChoose which Part/Function to execute:" << endl << endl
+		<< "1: Personal ID" << endl
+		<< "2: Annual Study Grant" << endl
+		<< "3: Choose between 2 functions" << endl
+		<< "4: Help" << endl
+		<< "5: Prime Number Check" << endl
+
+		<< "Q: Quit" << endl;
+}
+
 //Input asked info and outputs given info
-void Part1()
+void PersonalID()
 {
 	
 	string name;
@@ -76,7 +80,7 @@ void Part1()
 
 	
 	
-
+	cout << "\n*** PART 1***" << endl;
 	cout << "Input the following:\nName: "; cin >> name;
 	cout << "Age: "; cin >> age;
 	cout << "Student number: "; cin >> studentNum;
@@ -84,26 +88,29 @@ void Part1()
 	cout << "Weight: "; cin >> weight;
 
 	cout << "\nGiven info:\nName: " << name << "\nAge: " << age << "\nStudent ID: " << studentNum << "\nHeight: " << height << "\nWeight: " << weight << endl;	
+	cout << "\n*** PART 1 END***" << endl;
 }
 
 //input grant months per year and amount and outputs annual grant amount
-void Part2()
+void AnnualGrant()
 {
 	int months;
 	float grantAmount;
 	float grantPerYear;
+	cout << "\n*** PART 2***" << endl;
 	cout << "\nStudy grant months per year\nMonths: "; cin >> months;
 	cout << "Monthly grant amount: "; cin >> grantAmount;
 	grantPerYear = months * grantAmount;
 	cout << "Annual study grant amount: " << grantPerYear << "€" << endl;
+	cout << "\n*** PART 2 END***" << endl;
 }
-
-void Part3()
+//Choose betwenn two functions and practiced using switch cases
+void ChooseFunction()
 {
 	//making the ability to choose a function to do with a switch case
 	int choice;
-
-	cout << "\n1 - Convert Exam points to a grade\n2 - Convert distance from kilometers\n3 - Quit\nEnter your choice: ";
+	cout << "\n*** PART 3***" << endl;
+	cout << "\n1 - Convert Exam points to a grade\n2 - Convert distance from kilometers\n3 - Exit\nEnter your choice: ";
 	cin >> choice;
 
 	switch (choice) 
@@ -114,11 +121,13 @@ void Part3()
 		cout << "\n*** Points to Grades ***";
 		cout << "\nInsert exam points (0-100): "; cin >> points;
 
+
 		if (points < 0 || points > 100) {
-			cout << "\nInvalid points";
+			cout << "\nInvalid points. Gong back to main menu." << endl;
 		}
 		else {
-			int grade;
+			//cannot use a switch statements because the cases have to be constants and cannot be ranges of numbers, so using if else if for ranges is the best way
+			int grade;			
 			if (points >= 90) {
 				grade = 5;				
 				cout << "\nGrade" << grade;
@@ -153,22 +162,32 @@ void Part3()
 		cout << "\n-------------------------" << endl
 			<< "M - Convert to miles" << endl
 			<< "N - Convert to Nautical miles" << endl
-			<< "Q - Quit" << endl
+			<< "Q - Exit" << endl
 			<< "Enter your choice: ";
 		cin >> option;
-
-		if (option == 'm' || option == 'M') {
-			double miles = kilometers / 1.609;
-			cout << "\nThe distance in miles: " << fixed << setprecision(1) << miles << endl;
+		//switch case can execute the same code for different cases by nesting them like this
+		//works well for a very small range but becomes very impractical after a couple in a row and better off using if else if.
+		switch (option) {
+			case 'm':case 'M':
+			{
+				double miles = kilometers / 1.609;
+				cout << "\nThe distance in miles: " << fixed << setprecision(1) << miles << endl;
+				break;
+			}
+			case'n':case'N':
+			{
+				double nauticalMiles = kilometers / 1.852;
+				cout << "\nThe distance in nautical miles: " << fixed << setprecision(1) << nauticalMiles << endl;
+				break;
+			}
+			case 'q':case 'Q':
+			{
+				cout << "\nGoing back to Part 3 menu." << endl;
+				break;
+			}
+			default:
+				cout << "\nDistance in kilometers: " << kilometers << endl;
 		}
-		else if (option == 'n' || option == 'N') {
-			double nauticalMiles = kilometers / 1.852;
-			cout << "\nThe distance in nautical miles: " << fixed << setprecision(1) << nauticalMiles << endl;
-		}
-		else {
-			cout << "\nDistance in kilometers: " << kilometers << endl;
-		}
-			
 		break;
 	}
 	case 3: {
@@ -176,7 +195,32 @@ void Part3()
 		break;
 	}
 	default:
-	{ cout << "\nInvalid choice. Going back to main()" << endl; }
+	 cout << "\nInvalid choice. Going back to main menu." << endl; 
 	}
+	cout << "\n*** PART 3 END***" << endl;
 }
+//using for loops to check if a number is a primenumber
+void PrimeCheck() {
+	int primeNum;
+
+	cout << "\n*** PART 5***" << endl;
+	cout << "\nInput a number to check if it is prime or not: "; cin >> primeNum;	
 	
+	bool isPrime = true;
+	
+	for (int i = 2; i * i <= primeNum; i++) {//checking until the square root of the number given because checking more than that is redundant
+		if (primeNum % i == 0)
+		{
+			isPrime = false;
+			break;
+		}
+	}
+	if (isPrime) {
+		cout << "\nThe inputed number is a prime number." << endl;
+	}
+	else {
+		cout << "\nThe inputed number is NOT a prime number." << endl;
+	}
+	cout << "\n*** PART 5 END***" << endl;
+}
+
